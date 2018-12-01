0fe3495fef38d3acddcd2afb89491e670bb57d31
henry <bbosa.henry1@gmail.com>
2018-12-01 03:41:26 -0500

    securelink

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ export const API_AUTH_ID = "41ce8d6eb53e1633f9aeaeda6a814d6a";
 export const ZIP_REGEX = /^[0-9]{5}(?:-[0-9]{4})?$/;
 
 const API = axios.create({
-  baseURL: `http://api.openweathermap.org/data/2.5/`
+  baseURL: `https://api.openweathermap.org/data/2.5/`
 });
 
 export default API;
